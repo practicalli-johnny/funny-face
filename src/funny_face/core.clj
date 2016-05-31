fc52368aada249c436a821e215fa751081a8d2d5
John Stevenson <john@jr0cket.co.uk>
2016-05-31 20:48:10 +0100

    Working funny face
    Serves up a hard coded funny face

--- src/funny_face/core.clj
+++ src/funny_face/core.clj
@@ -4,7 +4,9 @@
             [compojure.core :refer [defroutes GET PUT]]
             [compojure.route :refer [not-found]]
             [hiccup.core :refer :all]
-            [hiccup.page :refer :all]))
+            [hiccup.page :refer :all]
+            [ring.middleware.resource :refer :all]
+            [clojure.java.io :as io]))
 
 (defn welcome
   "A ring handler to respond with a simple welcome message"
@@ -16,15 +18,23 @@
 (defn get-face
   "Returns a random face"
   [request]
+  (html5 {:lang "en"}
+         [:head (include-js "myscript.js") (include-css "mystyle.css")]
+         [:body
+          [:div [:h1 {:class "info"} "This is Hiccup"]]
+          [:div [:image "face.png"]]]))
+
+(defn ring-face
+  [request]
   {:status 200
-   :body "<img src=\"face.png\">"
-   :headers {}}
-  )
+   :body (io/file "face.png")
+   :headers {"Content-Type" "image/png"}})
 
 
 (defroutes app
   (GET "/" [] welcome)
   (GET "/face" [] get-face)
+  (GET "/ring-face" [] ring-face)
   (not-found "<h1>This is not the page you are looking for</h1>
               <p>Sorry, the page you requested was not found!</p>"))
 
