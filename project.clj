c13493149d8c1dcb7d04f26c9393b9615ba748ba
John Stevenson <john@jr0cket.co.uk>
2016-05-31 20:13:32 +0100

    We have a server and trying to get an image returned

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha3"]
+                 [org.clojure/test.check "0.9.0"]
+                 [ring "1.5.0-RC1"]
+                 [compojure "1.5.0"]
+                 [hiccup "1.0.5"]]
+  :main funny-face.core
+  :profiles {:dev
+             {:main funny-face.core/-dev-main}})
--- src/funny_face/core.clj
+++ src/funny_face/core.clj
@@ -1,6 +1,61 @@
-(ns funny-face.core)
+(ns funny-face.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET PUT]]
+            [compojure.route :refer [not-found]]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn welcome
+  "A ring handler to respond with a simple welcome message"
+  [request]
+  {:status 200
+   :body "<h1>Webservice placeholder</h1>"
+   :headers {}})
+
+(defn get-face
+  "Returns a random face"
+  [request]
+  {:status 200
+   :body "<img src=\"face.png\">"
+   :headers {}}
+  )
+
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/face" [] get-face)
+  (not-found "<h1>This is not the page you are looking for</h1>
+              <p>Sorry, the page you requested was not found!</p>"))
+
+(defn -main
+  "A very simple web server using Ring & Jetty"
+  [port-number]
+  (jetty/run-jetty app
+     {:port (Integer. port-number)}))
+
+(defn -dev-main
+  "A very simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
+  [port-number]
+  (jetty/run-jetty (wrap-reload #'app)
+     {:port (Integer. port-number)}))
+
+
+
+
+
+
+
+
+
+
+
+;; Reader macros
+;; (list 1 2 3 4)
+;; (quote  (1 2 3 4))
+;; '(1 2 3 4)
+#_(-> (str "fish")
+    #_(str "bla"))
+
+;;; (funtion
+;;      blah)
